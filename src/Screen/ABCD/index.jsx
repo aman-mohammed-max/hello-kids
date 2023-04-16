8365fb0d1d6dc5fc423ab70d94bcac7534f48057
aman-mohammed-max <74004117+aman-mohammed-max@users.noreply.github.com>
2023-04-16 22:03:49 +0530

    added firebase hosing

--- src/Screen/ABCD/index.jsx
+++ src/Screen/ABCD/index.jsx
@@ -1,5 +1,32 @@
+import { Box, Card, Grid, Heading } from "grommet";
 import React from "react";
 
 export default function ABCD() {
-  return <div>ABCD</div>;
+  return (
+    <Box height="100%" pad="medium">
+      <Grid
+        columns={{ count: "fill", size: "medium" }}
+        rows="medium"
+        gap="large"
+      >
+        <Card />
+        <Card
+          justify="center"
+          align="center"
+          onClick={() => navigate("/mathquiz")}
+        >
+          <Heading margin="none"> MathQuiz</Heading>
+        </Card>
+        <Card justify="center" align="center" onClick={() => {}}>
+          <Heading margin="none"> Slate</Heading>
+        </Card>
+        <Card justify="center" align="center" onClick={() => {}}>
+          <Heading margin="none"> ABCD</Heading>
+        </Card>
+
+        <Card />
+        <Card />
+      </Grid>
+    </Box>
+  );
 }
